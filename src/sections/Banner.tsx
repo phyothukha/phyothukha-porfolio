034b9c92288a97eda4d7b2bcab8ae2abe206813f
phyothukha <phyothukha2193@gmail.com>
2024-12-19 14:52:46 +0630

    fix(app): design layout in herosection

--- src/sections/Banner.tsx
+++ src/sections/Banner.tsx
@@ -5,38 +5,30 @@ import Link from "next/link";
 
 const Banner = () => {
   return (
-    <section
-      id="#me"
-      className="  bg-opacity-30  relative flex overflow-hidden "
-    >
-      <p
+    <section id="#me" className="  relative mt-16 overflow-hidden ">
+      <span
         aria-hidden="true"
-        className="bg-text absolute bottom-0 rotate-12 dark:text-[#3534344c] text-[#e7e7e7b4]  text-9xl scale-150 tracking-wide font-bold select-none pointer-events-none text-center -z-20"
+        className="bg-text absolute leading-tight top-36 rotate-12 text-gray-100 dark:text-[#4b4a4a31] text-[#e7e7e7b4] text-9xl scale-150 tracking-wide font-bold select-none pointer-events-none text-center -z-20"
       >
         PASSIONATE PROGRAMMER FREELANCER FULL-STACK DEVELOPER
-        <span className=" xl:inline-block hidden">
-          PASSIONATE PROGRAMMER FREELANCER FULL-STACK DEVELOPER
-        </span>
-        <span className=" xl:inline-block hidden">
-          PASSIONATE PROGRAMMER FREELANCER FULL-STACK DEVELOPER
-        </span>
-      </p>
+      </span>
       <div className=" w-full flex flex-col items-center lg:justify-start justify-evenly">
-        <div className=" flex lg:flex-row  flex-col-reverse gap-10  font-Roboto container max-w-6xl mx-auto items-center   lg:gap-5 justify-center  flex-wrap-reverse  lg:h-screen  lg:px-0 px-10  ">
-          <article className=" lg:w-1/2 self-center flex flex-col gap-5 lg:text-start text-center">
-            <p className=" lg:text-lg  text-sm font-normal font-Poetsen text-opacity-20 ">
+        <div className=" flex lg:flex-row items-center md:pb-40 pb-10  flex-col-reverse gap-5  font-Roboto container max-w-6xl mx-auto    lg:gap-5 justify-center  flex-wrap-reverse  min-h-[769px]  lg:px-0 px-10  ">
+          <article className=" lg:w-1/2 selection:bg-wtsecondary dark:selection:bg-secondary max-w-[500px]  flex flex-col gap-1 lg:gap-5 text-start ">
+            <p className=" lg:text-lg selection:text-primary text-sm font-normal font-Poetsen text-opacity-20 ">
               My name{"'"}s{" "}
-              <span className=" dark:text-secondary text-wtsecondary text-sm lg:text-lg font-Poetsen">
-                Phyo Thu Kha
-              </span>
             </p>
-            <h2 className=" lg:text-3xl text-2xl font-bold lg:font-semibold font-Ubuntu ">
+            <p className=" md:text-3xl text-xl dark:text-secondary dark:selection:text-darkary selection:text-primary text-wtsecondary lg:text-5xl font-Poetsen">
+              <span className="  font-Fira"> {"<"}</span>Phyo Thu Kha
+              <span className=" font-Fira">{"/>"}</span>
+            </p>
+            <h2 className=" lg:text-3xl text-2xl font-bold lg:font-semibold font-Ubuntu selection:text-primary dark:selection:text-darkary ">
               I am a{" "}
-              <span className=" dark:text-secondary text-wtsecondary italic font-bold font-Fira">
+              <span className=" dark:text-secondary  text-wtsecondary italic font-bold font-Fira">
                 MERN STACK DEVELOPER
               </span>
             </h2>
-            <p className="  tracking-widest font-Poetsen font-bold opacity-50 text-xs lg:text-sm">
+            <p className="  tracking-widest selection:text-primary dark:selection:text-darkary font-Poetsen font-bold text-xs lg:text-sm">
               I am a Full-stack developer skilled in handling diverse projects,
               learning new technologies, and solving user problems effectively.
             </p>
@@ -55,52 +47,52 @@ const Banner = () => {
               </button>
             </div>
           </article>
-          <aside className=" dark:bg-secondary bg-wtsecondary bg-opacity-70 lg:w-[450px] w-[300px] h-[300px] lg:h-[450px] rounded-full  transition ease-in-out duration-500 cursor-pointer overflow-hidden dark:shadow-3xl shadow-5xl hover:shadow-6xl dark:hover:shadow-4xl ">
+          <aside className=" dark:bg-secondary  bg-wtsecondary bg-opacity-70 lg:w-[450px] w-[300px] h-[300px] lg:h-[450px] rounded-full  transition ease-in-out duration-500 cursor-pointer overflow-hidden dark:shadow-3xl shadow-5xl hover:shadow-6xl dark:hover:shadow-4xl ">
             <div className=" absolute -bottom-50 "></div>
             <Image
               src={phyothukha}
-              className=" mt-20 lg:w-[500px] w-[300px] h-[300px] lg:h-[500px]"
+              className=" mt-20 mx-auto lg:w-[400px] w-[300px] h-[300px] lg:h-[400px]"
               alt="Phyo Thu Kha photo"
               priority
             />
           </aside>
         </div>
-        <div className=" animate  animate-bounce text-center">
-          <p>scroll</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="icon icon-tabler mx-auto icons-tabler-outline icon-tabler-mouse"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M6 3m0 4a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v10a4 4 0 0 1 -4 4h-4a4 4 0 0 1 -4 -4z" />
-            <path d="M12 7l0 4" />
-          </svg>
+      </div>
+      <div className=" absolute md:inline hidden  left-[50%] transform translate-x-[-50%] bottom-5  animate  animate-bounce text-center">
+        <p>scroll</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="icon icon-tabler mx-auto icons-tabler-outline icon-tabler-mouse"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M6 3m0 4a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v10a4 4 0 0 1 -4 4h-4a4 4 0 0 1 -4 -4z" />
+          <path d="M12 7l0 4" />
+        </svg>
 
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className=" mx-auto icon icon-tabler icons-tabler-outline icon-tabler-chevrons-down"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M7 7l5 5l5 -5" />
-            <path d="M7 13l5 5l5 -5" />
-          </svg>
-        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className=" mx-auto icon icon-tabler icons-tabler-outline icon-tabler-chevrons-down"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M7 7l5 5l5 -5" />
+          <path d="M7 13l5 5l5 -5" />
+        </svg>
       </div>
     </section>
   );
